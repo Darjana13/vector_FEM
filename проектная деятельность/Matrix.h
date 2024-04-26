136893cf101e5370c4db5049995c47a63ebd2c3f
leonovich <leonovich.d.a13@gmail.com>
2024-04-26 23:03:14 +0700

    Добавьте файлы проекта.

---
+++ проектная деятельность/Matrix.h
@@ -0,0 +1,17 @@
+#pragma once
+#include <vector>
+
+class Matrix
+{
+	
+public:	
+	int N;
+	std::vector<std::vector<double>> matrix;
+	Matrix operator * (double a);
+	Matrix operator + (Matrix B);
+
+
+	Matrix(int _N);
+	Matrix(); // matrix 4x4
+
+};
---
+++ проектная деятельность/Solver.h
@@ -0,0 +1,39 @@
+#pragma once
+#pragma once
+#include "MyMatrix.h"
+class Solver
+{
+private:
+    MyMatrix A;
+    MyVector p,
+        z,
+        r,
+        x0,
+        Ar,
+        y;
+    std::vector<double> L, D, U;
+    int N;
+    int maxIter;
+    double eps;
+    int iter;
+    double normR;
+    double normB;
+public:
+
+    Solver(int size);
+    Solver(std::string filename);
+    Solver(MyMatrix _A);
+    Solver(std::vector<int> ia, std::vector<int> ja,
+    std::vector<double> di, std::vector<double> al, std::vector<double> b);
+
+    void CGM_LU(); // conjugate gradient method whith LU factorization
+    void LOS_LU(); // locally optimal scheme whith LU factorization
+    void FactLU(std::vector<double>& L, std::vector<double>& U, std::vector<double>& D);
+    void Direct(std::vector<double>& L, std::vector<double>& D, MyVector& y, MyVector& b);
+    void Direct(std::vector<double>& L, MyVector& y, MyVector& b);
+    void Reverse(std::vector<double>& U, MyVector& x, MyVector& y);
+    void Reverse(std::vector<double>& U, std::vector<double>& D, MyVector& x, MyVector& y);
+    void output(std::string filename);
+    void getx0(std::vector<double>& x);
+
+};
